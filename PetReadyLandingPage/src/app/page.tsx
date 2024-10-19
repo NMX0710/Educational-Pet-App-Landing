b5390ab6d4094baca7b3ac1e7be07312016df2c1
Thota, Gowrav <gthota6@gatech.edu>
2024-10-19 18:47:51 -0400

    petready link

--- PetReadyLandingPage/src/app/page.tsx
+++ PetReadyLandingPage/src/app/page.tsx
@@ -90,7 +90,7 @@ export default function Component() {
               Teaching Responsibility in Pet Ownership
             </h1>
             <div className="flex justify-start gap-4">
-              <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-3xl shadow-md flex items-center">
+              {/* <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-3xl shadow-md flex items-center">
                 <svg
                   viewBox="0 0 24 24"
                   fill="currentColor"
@@ -104,8 +104,13 @@ export default function Component() {
                   </span>
                   <span className="text-lg font-bold">App Store</span>
                 </div>
-              </button>
-              <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-3xl shadow-md flex items-center">
+              </button> */}
+              <button
+                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-3xl shadow-md flex items-center"
+                onClick={() => {
+                  window.location.href = 'https://play.google.com/store/apps/details?id=com.anonymous.petready&hl=en_US';
+                }}
+              >
                 <svg
                   viewBox="0 0 24 24"
                   fill="currentColor"
@@ -114,7 +119,7 @@ export default function Component() {
                   <path d="M3,20.5V3.5C3,2.91 3.34,2.39 3.84,2.15L13.69,12L3.84,21.85C3.34,21.6 3,21.09 3,20.5M16.81,15.12L6.05,21.34L14.54,12.85L16.81,15.12M20.16,10.81C20.5,11.08 20.75,11.5 20.75,12C20.75,12.5 20.5,12.92 20.16,13.19L17.89,14.5L15.39,12L17.89,9.5L20.16,10.81M6.05,2.66L16.81,8.88L14.54,11.15L6.05,2.66Z" />
                 </svg>
                 <div className="flex flex-col">
-                  <span className="text-xs font-medium">Coming soon on</span>
+                  <span className="text-xs font-medium">Available on</span>
                   <span className="text-lg font-bold">Google Play</span>
                 </div>
               </button>
